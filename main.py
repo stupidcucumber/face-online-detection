1ee8390e420f119e97672c9522182755d1a29f55
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2025-04-02 07:54:50 +0300

    chore: implemented inferencing with faster-rcnn

--- main.py
+++ main.py
@@ -39,15 +39,31 @@ def predict_faster_rcnn(
     detector: torch.nn.Module,
     width: int = 512,
     height: int = 512
-) -> None:
+) -> cv2.Mat:
     
     img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB).astype(np.float32)
     img_res = cv2.resize(img_rgb, (width, height), cv2.INTER_AREA) / 255.0
-    image = torch.as_tensor(img_res, dtype=torch.float32).permute(2, 0, 1)
+    image = torch.as_tensor(img_res, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)
     
-    predictions = detector(image)
+    with torch.no_grad():
+        
+        predictions = detector(image)[0]
     
-    print(predictions)
+    boxes = predictions["boxes"].detach().cpu().numpy()
+
+    frame = cv2.resize(frame, (width, height))
+    
+    for xmin, ymin, xmax, ymax in boxes:
+        
+        cv2.rectangle(
+            frame,
+            (int(xmin), int(ymin)),
+            (int(xmax), int(ymax)),
+            [0, 255, 0],
+            2
+        )
+        
+    return frame
 
 
 def main(model: Literal["yolo", "faster-rcnn"], weights: Path, video_capture_address: str | int) -> None:
@@ -59,11 +75,15 @@ def main(model: Literal["yolo", "faster-rcnn"], weights: Path, video_capture_add
     else:
         
         detector = get_object_detection_model(2, weights)
+        detector.eval()
     
     cap = cv2.VideoCapture(video_capture_address)
     
     while cap.isOpened():
+        
         captured, frame = cap.read()
+        
+        frame_height, frame_width = frame.shape[:2]
         
         if captured:
             
@@ -73,7 +93,13 @@ def main(model: Literal["yolo", "faster-rcnn"], weights: Path, video_capture_add
             
             else:
                 
-                predictions = ...
+                predicted_image = predict_faster_rcnn(frame, detector)
+                
+                predicted_frame = cv2.resize(predicted_image, (frame_width, frame_height))
+                
+                cv2.imshow("Detecting faces", predicted_frame)
+                
+                cv2.waitKey(1)
 
 
 if __name__ == "__main__":
